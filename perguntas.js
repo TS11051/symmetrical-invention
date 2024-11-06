a2ca781cdb9753b0f79ce28214b350d5085aa4ef
TS11051 <00001105135160sp@al.educacao.sp.gov.br>
2024-11-06 11:07:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem baseada em regras'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Itália?',
+    'A capital da Itália é Roma'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'O Java e uma linguagem de programação?'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amor em Inglês?',
+    'amorem ingles é Love'
+)
